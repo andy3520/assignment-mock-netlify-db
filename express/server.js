193d2bae598eb2ab99958fac4f930f20283e111a
An Nguyen <an.nguyen@emerhub.com>
2020-03-26 11:52:58 +0700

    update policy

--- express/server.js
+++ express/server.js
@@ -6,15 +6,29 @@ const app = express();
 const bodyParser = require('body-parser');
 const db = require('../db.json')
 const cors = require('cors')
-
+const router = express.Router();
 
 app.use(cors())
-const router = express.Router();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", CLIENT_ORIGIN);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.header("Access-Control-Allow-Credentials", true); <--- this is the only different line I added.
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 router.get('/top-company', (req, res) => {
   res.json(db['top-company'])
 })
 
-router.get('/company',cors(), (req, res) => {
+router.get('/company', (req, res) => {
   res.json(db['company'])
 })
 
