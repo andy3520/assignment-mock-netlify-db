6293030ef5f9ff76e5cceb9adccf273a0b29a65f
An Nguyen <an.nguyen@emerhub.com>
2020-03-26 11:53:39 +0700

    update policy

--- express/server.js
+++ express/server.js
@@ -11,7 +11,7 @@ const router = express.Router();
 app.use(cors())
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", CLIENT_ORIGIN);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
